7a52d8b0d11a239098e5459f027d59f539723fe1
Darcy Murphy <mrDarcyMurphy@gmail.com>
2012-03-10 06:35:42 -0600

    upfront grabs recognized templates and stores them under app.settings.templates

--- test/upfront.js
+++ test/upfront.js
@@ -43,9 +43,43 @@ describe('Successful Setup', function(){
   it('succeeds when everything is setup right', function(done){
     should.exist(app.settings);
     should.exist(app.settings.views);
-    upfront.setup(app, function(err, succeeded){
+    upfront.setup(app, function(err, success){
       should.not.exist(err);
-      should.exist(succeeded);
+      should.exist(success);
+      done();
+    });
+  });
+
+});
+
+
+describe('Template Compilation', function(){
+
+  var app;
+
+  before(function(done){
+    app = module.exports = express.createServer();
+    app.configure(function(){
+      app.set('views', __dirname + '/views');
+    });
+    upfront.setup(app, function(err, success){
+      should.not.exist(err);
+      should.exist(success);
+      done();
+    });
+  });
+
+  it('walks the view tree and loads views into app.settings.templates', function(done){
+    upfront.grab(function(err, success){
+      should.not.exist(err);
+      should.exist(success);
+      should.exist(app.settings.templates);
+      should.exist(app.settings.templates.md);
+      should.exist(app.settings.templates.html);
+      should.exist(app.settings.templates.utml);
+      should.exist(app.settings.templates['subone/nested']);
+      should.exist(app.settings.templates['subone/subtwo/nested2']);
+      should.not.exist(app.settings.templates.unrecognized);
       done();
     });
   });
