8f7ab9335d3f4e21be05268d3ddd80f753d040dc
Darcy Murphy <mrDarcyMurphy@gmail.com>
2012-03-11 11:40:38 -0500

    converted the initial argument to an object to allow for a custom configuration file to be passed in.

--- test/upfront.js
+++ test/upfront.js
@@ -9,7 +9,7 @@ describe('Botched Setup', function(){
 
   it('fails gracefully when app is undefined', function(done){
     should.not.exist(app);
-    upfront.setup(app, function(err, result){
+    upfront.setup({app:app}, function(err, result){
       should.exist(err);
       done();
     });
@@ -18,7 +18,7 @@ describe('Botched Setup', function(){
   it('fails gracefully when view path is undefined', function(done){
     app = module.exports = express.createServer();
     should.exist(app.settings);
-    upfront.setup(app, function(err, result){
+    upfront.setup({app:app}, function(err, result){
       should.exist(err);
       done();
     });
@@ -42,7 +42,7 @@ describe('Successful Setup', function(){
   it('succeeds when everything is setup right', function(done){
     should.exist(app.settings);
     should.exist(app.settings.views);
-    upfront.setup(app, function(err, success){
+    upfront.setup({app:app}, function(err, success){
       should.not.exist(err);
       should.exist(success);
       done();
@@ -58,7 +58,7 @@ describe('Configuration', function(){
     app.configure(function(){
       app.set('views', __dirname + '/views');
     });
-    upfront.setup(app, function(err, success){
+    upfront.setup({app:app}, function(err, success){
       should.not.exist(err);
       should.exist(success);
       done();
@@ -87,7 +87,7 @@ describe('Template Compilation', function(){
     app.configure(function(){
       app.set('views', __dirname + '/views');
     });
-    upfront.setup(app, function(err, success){
+    upfront.setup({app:app}, function(err, success){
       should.not.exist(err);
       should.exist(success);
       upfront.grab(function(err, success){
@@ -119,3 +119,47 @@ describe('Template Compilation', function(){
   });
 
 });
+
+
+describe('Custom Template Compilation', function(){
+
+  var app, upfront = require('../lib/upfront.js');
+
+  before(function(done){
+    app = module.exports = express.createServer();
+    app.configure(function(){
+      app.set('views', __dirname + '/views');
+    });
+    upfront.setup({app:app}, function(err, success){
+      should.not.exist(err);
+      should.exist(success);
+      upfront.grab(function(err, success){
+        done();
+      });
+    });
+  });
+
+  it('ignores upfront.json', function(){
+    should.not.exist(app.settings.templates.upfront);
+  });
+
+  it('loads views into app.settings.templates', function(){
+    should.exist(app.settings.templates);
+    should.exist(app.settings.templates['extension_md']);
+    should.exist(app.settings.templates['extension_html']);
+    should.exist(app.settings.templates['extension_utml']);
+    should.exist(app.settings.templates['subone/nested']);
+    should.exist(app.settings.templates['subone/subtwo/nested2']);
+  });
+
+  it('ignores unrecognized files', function(){
+    should.not.exist(app.settings.templates['unrecognized']);
+    should.not.exist(app.settings.templates['extension_ejs']);
+  });
+
+  it('ignores files listed in upfront.json', function(){
+    should.not.exist(app.settings.templates.ignore_me);
+  });
+
+});
+
